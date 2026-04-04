e8258edfd3ac62b4caf325a38a4619bd59f75781
ApostolOleg <apostololeg0@gmail.com>
2026-04-04 21:48:39 +0300

    refactor: split user controller and services into dedicated files for improved organization (#47)

--- src/modules/users/index.js
+++ src/modules/users/index.js
@@ -1,4 +1,4 @@
-export { default as userRoutes } from './user.routes.js';
-export * as userController from './user.controller.js';
-export * as userServices from './user.services.js';
-export { default as User } from './user.model.js';
+export { default as userRoutes } from "./user.routes.js";
+export * as userController from "./controllers/user.js";
+export * as userServices from "./services/user.js";
+export { default as User } from "./user.model.js";
